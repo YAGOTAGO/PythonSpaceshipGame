554aa8bb1cd74de2f85816ed93245979aaef7076
YAGOTAGO <tiagocaetanodavies@gmail.com>
2022-10-11 22:24:05 -0400

    Added main menu and explosion

--- ExecuteGame.py
+++ ExecuteGame.py
@@ -1,5 +1,6 @@
 from pickle import TRUE
 from random import randint
+from tkinter import EventType
 import pygame
 from sys import exit
 
@@ -8,9 +9,11 @@ WIDTH = 1300
 HEIGHT = 800
 TOPBOUND = 70
 FPS = 60
+UICENTERY = 40
+
+#pygame scree
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-UICENTERY = 40
 
 #spawn vars
 SPAWNX = WIDTH + 100
@@ -38,6 +41,17 @@ fontSize = 50
 gameFont = pygame.font.Font('Fonts\AudiowideFont.ttf', fontSize)
 startTime = 0
 
+#explosion images
+explosion1 = pygame.image.load('Images\explosion\explosion1.png').convert_alpha()
+explosion2 = pygame.image.load('Images\explosion\explosion2.png').convert_alpha()
+explosion3 = pygame.image.load('Images\explosion\explosion3.png').convert_alpha()
+explosion4 = pygame.image.load('Images\explosion\explosion4.png').convert_alpha()
+explosion5 = pygame.image.load('Images\explosion\explosion5.png').convert_alpha()
+explosion6 = pygame.image.load('Images\explosion\explosion6.png').convert_alpha()
+explosionIndex = 0
+explosionFrames = [explosion1, explosion2, explosion3, explosion4, explosion5, explosion6]
+explodeOnce = True
+
 #coins images
 coin1 = pygame.image.load('Images\coins\Gold1.png').convert_alpha()
 coin2 = pygame.image.load('Images\coins\Gold2.png').convert_alpha()
@@ -64,12 +78,16 @@ maxHeartCD = 700 # cd after first spawn
 
 #fuel
 fuelImg = pygame.image.load('Images\FuelCanister.png').convert_alpha()
+redFuelImg = pygame.image.load('Images\Fuel.png').convert_alpha()
 fuelRect = fuelImg.get_rect(center = (600, UICENTERY))
 fuelRecoverAmount = 1000
 fuelCD = 2000
 maxFuelCd = 2000
 
-#Groups
+#main menu
+mainMenuImg = pygame.image.load('Images\startBackground.png').convert_alpha()
+
+#bullet groups
 bulletGroup = pygame.sprite.Group()
 
 #sets display variables
@@ -120,10 +138,10 @@ class SpaceShip(pygame.sprite.Sprite):
 
     def update(self):
         global startTime
+
         if(self.health <=0):
-            self.kill()
-            self.alive = False
-            startTime = int (pygame.time.get_ticks()/ 1000) ## when making restart game function this will be how to reset the time
+            self.health = 0
+            self.onPlayerDeath()
 
         #movement
         if thrustersOn:
@@ -134,8 +152,8 @@ class SpaceShip(pygame.sprite.Sprite):
             self.shoot()
 
         #decreases shot cooldown
-        if self.shootCD>0:
-            self.shootCD-=1
+        if self.shootCD > 0:
+            self.shootCD -= 1
 
         #speed of spaceship
         self.velocity = (self.damping * self.velocity) + self.gravity
@@ -149,6 +167,10 @@ class SpaceShip(pygame.sprite.Sprite):
         elif self.rect.top <= TOPBOUND:
             self.rect.top = TOPBOUND
         
+    def onPlayerDeath(self):
+        self.kill()
+        self.alive = False
+    
     def MoveUp(self):
         if self.fuel>=0:
             self.velocity -= self.thrust
@@ -380,6 +402,8 @@ EnemiesGroup = pygame.sprite.Group()
 EnemiesGroup.add(dripDude)
 
 
+#spawn collectable functions
+
 def spawnCoin():
     global coinCD
     if coinCD == 0:
@@ -410,7 +434,7 @@ def spawnHeart():
 def spawnFuel():
     global fuelCD
     if fuelCD == 0:
-        fuel = Fuel(fuelImg)
+        fuel = Fuel(redFuelImg)
         fuelGroup.add(fuel)
         fuelCD = maxFuelCd
     else:
@@ -422,9 +446,22 @@ def displayInfo(x, y, info):
     timeRect = timeSurf.get_rect(center = (x,y))
     screen.blit(timeSurf, timeRect)
 
+#method for explosion
+def explosion(rect):
+    global explosionIndex, explodeOnce
+    
+    if explodeOnce:
+        if explosionIndex <= len(explosionFrames):
+            screen.blit(explosionFrames[int(explosionIndex)], rect)
+            explosionIndex += .2
+        else:
+            explosionIndex = 0
+            explodeOnce = False
+        
 
+    
 
-#method for event handling
+#method for event handling during the game
 def eventLoop():
     global thrustersOn, shoot
 
@@ -446,77 +483,98 @@ def eventLoop():
             if event.key == pygame.K_SPACE: # shooting (space)
                 shoot = False
 
+#MainScreen
 
 #Main gameplay loop
 gameOver = False
-while not gameOver:
-    
-    eventLoop() #registers all events
 
-    #scroll background
-    for i in range(0,2):
-        screen.blit(bg, (i * bgWidth + scroll, 0)) # draw background
+def play():
+    global scroll
+    while not gameOver:
 
-    #reset scroll
-    if abs(scroll)>bgWidth:
-        scroll = 0
-    scroll -= 5
+        eventLoop() #registers all events
 
-    #displays information
-    #displayInfo(100,40, int (pygame.time.get_ticks()/ 1000) - startTime) # displays the time in seconds
-    
-    #displays health UI
-    screen.blit(heartImg, heartRect)
-    displayInfo(200, UICENTERY, spaceship.health)
+        #scroll background
+        for i in range(0,2):
+            screen.blit(bg, (i * bgWidth + scroll, 0)) # draw background
 
-    #display ammo UI
-    screen.blit(blueBullet, blueBulletRect)
-    displayInfo(445, UICENTERY, spaceship.ammo)
+        #reset scroll
+        if abs(scroll)>bgWidth:
+            scroll = 0
+        scroll -= 5
 
-    #display fuel UI
-    screen.blit(fuelImg, fuelRect)
-    displayInfo(710, UICENTERY, spaceship.fuel)
+        #displays information
+        #displayInfo(100,40, int (pygame.time.get_ticks()/ 1000) - startTime) # displays the time in seconds
+        
+        #displays health UI
+        screen.blit(heartImg, heartRect)
+        displayInfo(200, UICENTERY, spaceship.health)
 
-    #display score UI
-    screen.blit(coin1, coin1Rect)
-    displayInfo(1100, UICENTERY, spaceship.score)
+        #display ammo UI
+        screen.blit(blueBullet, blueBulletRect)
+        displayInfo(445, UICENTERY, spaceship.ammo)
 
-    #update and draw groups
+        #display fuel UI
+        screen.blit(fuelImg, fuelRect)
+        displayInfo(710, UICENTERY, spaceship.fuel)
 
-    #spaceship group
-    spaceshipGroup.update() 
-    spaceshipGroup.draw(screen)
-    
-    #coins
-    spawnCoin()
-    coinGroup.update()
-    coinGroup.draw(screen)
+        #display score UI
+        screen.blit(coin1, coin1Rect)
+        displayInfo(1100, UICENTERY, spaceship.score)
 
-    #heart
-    spawnHeart()
-    heartGroup.update()
-    heartGroup.draw(screen)
+        #update and draw groups
 
-    #spawn ammo
-    spawnAmmo()
-    ammoGroup.update()
-    ammoGroup.draw(screen)
+        #spaceship group
+        spaceshipGroup.update() 
+        spaceshipGroup.draw(screen)
+        
+        #coins
+        spawnCoin()
+        coinGroup.update()
+        coinGroup.draw(screen)
 
-    #fuel
-    spawnFuel()
-    fuelGroup.update()
-    fuelGroup.draw(screen)
+        #heart
+        spawnHeart()
+        heartGroup.update()
+        heartGroup.draw(screen)
 
-    #enemies
-    EnemiesGroup.update()
-    EnemiesGroup.draw(screen)
+        #spawn ammo
+        spawnAmmo()
+        ammoGroup.update()
+        ammoGroup.draw(screen)
 
-    #bullet group
-    bulletGroup.update()
-    bulletGroup.draw(screen)
+        #fuel
+        spawnFuel()
+        fuelGroup.update()
+        fuelGroup.draw(screen)
 
-    pygame.display.update() #maintains display open
-    clock.tick(FPS) #sets fram rate to 60
+        #enemies
+        EnemiesGroup.update()
+        EnemiesGroup.draw(screen)
+
+        #bullet group
+        bulletGroup.update()
+        bulletGroup.draw(screen)
+
+        if(spaceship.alive == False):
+            explosion(spaceship.rect.topleft)
+
+        pygame.display.update() #maintains display open
+        clock.tick(FPS) #sets fram rate to 60
 
 
+def mainMenu():
+    while True:
+        screen.blit(mainMenuImg, (0,0)) #Draws Main Menu
 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_s: #if key S is pressed then start the game
+                    play()
+
+        pygame.display.update()
+
+mainMenu()
